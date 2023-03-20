6067e02bc7550e10ef5ca38b0a57fff6e6fcb1e8
arden wang <ardentwang@gmail.com>
2023-03-20 14:00:44 -0500

    Latest Version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+import About from './routes/about/about.component';
+import Contact from './routes/contact/contact.component';
+import Projects from './routes/projects/projects.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Navigation/>}>
+          <Route index element={<Home/>} />
+          <Route path='about' element={<About/>} />
+          <Route path='contact' element={<Contact/>} />
+          <Route path='projects' element={<Projects/>} />
+          <Route path='work' element={<About/>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,46 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import "./navigation.component.scss";
+import { ReactComponent as HomeLogo } from '../../assets/house.svg';
+import { ReactComponent as WorkLogo } from '../../assets/work.svg';
+import { ReactComponent as InfoLogo } from '../../assets/info.svg';
+import { ReactComponent as ContactLogo } from '../../assets/phone.svg';
+import { ReactComponent as ProjectLogo } from '../../assets/projects.svg';
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+              <div className='nav-links-container'>
+                <Link className='nav-link' to='/'>
+                  <HomeLogo className='logo-container'/>
+                  Home
+                </Link>
+
+                <Link className='nav-link' to='about'>
+                  <InfoLogo className='logo-container'/>
+                  About
+                </Link>
+
+                <Link className='nav-link' to='projects'>
+                  <ProjectLogo className='logo-container'/>
+                  Projects
+                </Link>
+
+                <Link className='nav-link' to='work'>
+                  <WorkLogo className='logo-container'/>
+                  Work
+                </Link>
+
+                <Link className='nav-link' to='contact'>
+                  <ContactLogo className='logo-container'/>
+                  Contact
+                </Link>
+              </div>
+            </div>
+            <Outlet/>
+        </Fragment>
+    )
+}
+
+export default Navigation;
