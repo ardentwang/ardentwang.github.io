b910c927207611b8d332178a5a7a38a99b1425ec
arden wang <ardentwang@gmail.com>
2023-09-26 14:30:09 -0500

    Updated home and projects routes

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -7,22 +7,17 @@ const Home = () => {
         <div className='home'>
             <section className='hero-section'>
                 <h1>Welcome to My Website</h1>
-                <p>This is a brief description of what we do. You can add more details here to give your visitors a quick overview of your services or products.</p>
-            </section>
-            <section className='featured-section'>
-                <h2>Featured Items</h2>
-                <div className='featured-items'>
-                    {/* You can map over your featured items here and display them */}
-                    <div className='featured-item'>
-                        <h3>Featured Item 1</h3>
-                        <p>Description for featured item 1.</p>
-                    </div>
-                    <div className='featured-item'>
-                        <h3>Featured Item 2</h3>
-                        <p>Description for featured item 2.</p>
-                    </div>
-                    {/* Add more featured items as needed */}
-                </div>
+                <p>
+                    Welcome! I'm a junior at the University of Wisconsin Madison, 
+                    actively pursuing a double major in Computer Science and Data Science. 
+                    With a passion for AI, I currently serve as an Artificial Intelligence Researcher, 
+                    diving deep into machine learning algorithms and decision-making processes. 
+                    My experience ranges from software engineering internships at California ISO to teaching computer science with Juni Learning. 
+                    I've also had the opportunity to work in data science in a biology setting at UCLA, adding a multidisciplinary flair to my skillset. 
+                    With proven expertise in Python, Java, and JavaScript, I not only code but also empower others to learn, boasting a 100% pass rate for my mentored students. 
+                    Whether it's identifying lifestyle risk factors through machine learning or presenting technical and financial demos to corporate stakeholders, I thrive at the intersection of technology, education, and actionable insights. 
+                    Take a look around to explore my projects, research, and more.
+                </p>
             </section>
             <footer className="contact-footer">
                 <Contact />
--- src/routes/projects/projects.component.jsx
+++ src/routes/projects/projects.component.jsx
@@ -1,8 +1,27 @@
+// Projects.js
 import React from 'react';
+import Project from '../../components/project.js'; // Import the new component
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './projects.scss';
+import amazonImage from '../../assets/images/amazon.png';
+import covidImage from '../../assets/images/covid.png';
 
 const Projects = () => {
+  const projectData = [
+    { 
+      image: amazonImage, 
+      title: 'Amazon Clone', 
+      description: 'The Amazon Clone website aims to replicate the key functionalities and design aspects of the original Amazon platform. Built with the end-user in mind, this clone provides a shopping experience that aims to mirrors the simplistic portions Amazon.com. ',
+      link: 'https://atwang-amzclone.web.app'
+    },
+    { 
+      image: covidImage, 
+      title: 'COVID-19 Tracker', 
+      description: 'The Global COVID-19 Tracker is a React-based web application designed to provide real-time, reliable statistics on the COVID-19 pandemic, focusing specifically on the number of deaths and infections in each country. Utilizing a clean, user-friendly interface, the application integrates data from reputable sources to offer a comprehensive view of the pandemic\'s global impact. ',
+      link: 'https://california-covid-19-tracker.web.app/'
+    },
+  ];
+
   return (
     <div className="projects-page">
       <header className="projects-header">
@@ -10,15 +29,15 @@ const Projects = () => {
       </header>
 
       <section className="projects-content">
-        <div className="project-card">
-          <h2>Project 1</h2>
-          <p>Description for Project 1.</p>
-        </div>
-        <div className="project-card">
-          <h2>Project 2</h2>
-          <p>Description for Project 2.</p>
-        </div>
-        {/* Add more project cards as needed */}
+        {projectData.map((proj, index) => (
+          <Project
+            key={index}
+            image={proj.image}
+            title={proj.title}
+            description={proj.description}
+            link={proj.link}  // Pass the link prop here
+          />
+        ))}
       </section>
     </div>
   );
